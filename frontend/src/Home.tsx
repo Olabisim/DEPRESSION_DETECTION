dd43dcf6a64d9813326483ba0b4bf1623f821107
holabisii <ajoseholabisi@gmail.com>
2022-06-10 14:13:45 -0400

    home header done

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,9 +1,42 @@
 
+import './css/home.css'
+
 
 export const Home = () => {
         return (
                 <div>
-                        
+                        <div className="home_main">
+                                <header className="home_header">
+
+                                        <div className="home_headerimgbx">
+                                                <div className="home_header-im">
+                                                        <img  className="home_headerimg" src="https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZXhhbXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                                                        <div className='main_home_textbx'>
+                                                                
+                                                                <div className="home_textbx">
+                                                                        A self test can help you <br /> decide <br /> what to do.
+                                                                </div>
+
+                                                        </div>
+                                                </div>
+                                        </div>
+
+                                        
+                                </header>
+                        </div>
+
+                        {/* <div>
+
+                                <div className="home_header-rig">
+                                        <div className="home_headertext-bx">
+                                                <h1 className="home_title">Where dimensions take shape
+                                                </h1>
+                                                <p className="txt">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+                                                <a href="#s" className="buttn">Read More</a>
+                                        </div>
+                                </div>
+
+                        </div> */}
                 </div>
         )
 }
